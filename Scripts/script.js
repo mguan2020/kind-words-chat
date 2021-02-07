dd8bb67af80f8dfdf36a5e5c5494793bc0362d6c
MuhenderRaj <rkmr02@gmail.com>
2021-02-07 12:17:43 +0530

    Finished most of frontend and backend

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+//Open and close the sidebar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
